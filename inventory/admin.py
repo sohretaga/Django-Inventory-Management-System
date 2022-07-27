09f108cb673180c3310f7f7d9c9f19b31d69279d
sohretaga <shohretaga@gmail.com>
2022-07-28 03:13:14 +0400

    Created order model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import Group
 # Register your models here.
 
@@ -9,5 +9,10 @@ class ProductAdmin(admin.ModelAdmin):
   list_display = ('name', 'category', 'quantity')
   list_filter = ('category',)
 
+class OrderAdmin(admin.ModelAdmin):
+  list_display = ('product', 'staff', 'date')
+  list_filter = ('product', 'staff', 'date')
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.unregister(Group)
--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,5 @@
-from sre_parse import CATEGORIES
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY = (
@@ -15,3 +15,12 @@ class Product(models.Model):
 
   def __str__(self) -> str:
     return self.name
+
+class Order(models.Model):
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+  staff = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+  order_quantity = models.PositiveIntegerField(null=True)
+  date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.product.name
